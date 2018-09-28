194ae9895f5df5eb9aeaeccfad41f5e8e104b8cf
andrey <jeremy1.andrey@epitech.eu>
2018-09-28 16:53:31 +0200

    add express server to send html page and 3d models to clients

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const openvr = require("node-openvr");
-var vr = null;
-try{
-    vr = openvr.system.VR_Init(openvr.EVRApplicationType.Background)
-}catch(e) {
-    vr = null;
-    console.error("VR Error", e);
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.redirect('/index.html'))
+app.use(express.static('static'))
+const server = app.listen(9090)
+
+const openvr = require("openvr");
+openvr.k_unMaxTrackedDeviceCount = 64;
+var vr = openvr.VR_Init(openvr.EVRApplicationType.Background)
+
+function convert_to_quaternion(_pose_mat) {
+    let pose_mat = Array.from(_pose_mat)
+    let r_w = Math.sqrt(Math.abs(1 + pose_mat[0][0] + pose_mat[1][1] + pose_mat[2][2]))/2
+    let r_x = (pose_mat[2][1]-pose_mat[1][2])/(4*r_w)
+    let r_y = (pose_mat[0][2]-pose_mat[2][0])/(4*r_w)
+    let r_z = (pose_mat[1][0]-pose_mat[0][1])/(4*r_w)
+
+    let x = pose_mat[0][3]
+    let y = pose_mat[1][3]
+    let z = pose_mat[2][3]
+    return {
+	'x':x,
+	'y':y,
+	'z':z,
+	'qw':r_w,
+	'qx':r_x,
+	'qy':r_y,
+	'qz':r_z
+    };
 }
 
 const WebSocket = require('ws');
@@ -39,55 +62,51 @@ wss.on('error', function(event){
     console.error("SRV Error", event);    
 })
 
-
-function convert_to_quaternion(_pose_mat) {
-    pose_mat = Array.from(_pose_mat)
-    let r_w = Math.sqrt(Math.abs(1 + pose_mat[0] + pose_mat[5] + pose_mat[10]))/2
-    let r_x = (pose_mat[6]-pose_mat[9])/(4*r_w)
-    let r_y = (pose_mat[8]-pose_mat[2])/(4*r_w)
-    let r_z = (pose_mat[1]-pose_mat[4])/(4*r_w)
-
-    let x = pose_mat[3]
-    let y = pose_mat[7]
-    let z = pose_mat[11]
-    return [x,y,z,r_w,r_x,r_y,r_z]
-}
-
-
-let matrice = new Float32Array(16);
-
 const interval = setInterval(function updateClientsPosition() {
-    if (vr !== null) {
-	// get data from VIVE !!!
-	console.log(vr.GetDeviceToAbsoluteTrackingPose(openvr.ETrackingUniverseOrigin.Standing, 0))
-
-    } else {
-	// generate data for testing and demo
+    let poses = vr.GetDeviceToAbsoluteTrackingPose(openvr.ETrackingUniverseOrigin.Standing, 0)
+    for (let i = 0; i < openvr.k_unMaxTrackedDeviceCount; ++i) {
+	if (poses[i] !== undefined) {
+	    switch (vr.GetTrackedDeviceClass(i)) {
+	    case openvr.ETrackedDeviceClass.Invalid:
+		//console.error('INVALID DEVICE');
+		break;
+	    case openvr.ETrackedDeviceClass.HMD:
+		//console.log('HMD');
+	    // 	break;
+	    // case openvr.ETrackedDeviceClass.Controller:
+	    // 	console.log('CONTROLLER');
+		//console.log(poses[i])
+		let quat = convert_to_quaternion(poses[i].deviceToAbsoluteTracking)
+		let msg = JSON.stringify(quat)
+		wss.clients.forEach(function each(ws) {
+		    if (ws.readyState == ws.OPEN)
+			ws.send(msg);
+		})
+		return;
+		    //break;
+	    case openvr.ETrackedDeviceClass.GenericTracker:
+		//console.log('GENERIC TRACKER');
+		break;
+	    case openvr.ETrackedDeviceClass.TrackingReference:
+		//console.log('TRACKING REFERENCE');		
+		break;
+	    case openvr.ETrackedDeviceClass.DisplayRedirect:
+		//console.log('DISPLAY REDIRECT');
+		break;
+	    default:
+		console.error("ERROR UNKNOWN DEVICE")
+	    }
+	}
     }
-
-    let quat = convert_to_quaternion(matrice);
-    wss.clients.forEach(function each(ws) {
-	ws.send(JSON.stringify({
-	    'x': quat[0],
-	    'y': quat[1],
-	    'z': quat[2],
-	    'rw':quat[3],
-	    'rx':quat[4],
-	    'ry':quat[5],
-	    'rz':quat[6]	    
-	}));
-    });
-}, 500);
+}, 50);
 
 function shutdown(code) {
-    console.log('SIGINT received...');
+    console.log('SIGNAL received...');
     clearInterval(interval);
     wss.close();
-    if (vr) {
-	openvr.system.VR_Shutdown();
-    }
+    server.close();
+    openvr.VR_Shutdown();
 };
 
 process.once('SIGINT', shutdown);
 process.once('SIGTERM', shutdown);
-//shutdown();
