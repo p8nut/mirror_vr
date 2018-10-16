73902240b889de67bef2abb683317a82922814ad
Victor Beltrami <victorbeltrami@localhost.localdomain>
2018-10-16 18:38:55 +0200

    [ADD] easter egg on Sun

--- static/js/Sun.js
+++ static/js/Sun.js
@@ -1,8 +1,8 @@
+var popup = PopUp.getInstance();
+
 class Sun extends BasicEntity {
   constructor(univers) {
-    super(univers, new THREE.CubeGeometry(0.1, 16, 8));
-    // sun.position.y = - 700000;
-    // sun.visible = false;
+    super(univers, new THREE.CubeGeometry(1000, 1000, 1000));
     this.add(new THREE.PointLight(0xf0f0f0, 10, 100000));
     
     this.textureLoader = new THREE.TextureLoader();
@@ -11,7 +11,7 @@ class Sun extends BasicEntity {
 
     var light = new THREE.PointLight( 0xffffff, 1.5, 2000 );
     light.color.setHSL( 0.08, 0.8, 0.5 );
-    light.position.set( 0, 0, -1000 );
+    light.position.set( 0, 0, 0 );
     this.add( light );
 
     var lensflare = new THREE.Lensflare();
@@ -21,5 +21,8 @@ class Sun extends BasicEntity {
     lensflare.addElement( new THREE.LensflareElement( this.textureFlare3, 120, 0.9 ) );
     lensflare.addElement( new THREE.LensflareElement( this.textureFlare3, 70, 1 ) );
     light.add( lensflare );
-  }
+	}
+	mouseClick(event, elapsedTime) {
+		popup.send("Don't touch the Sun :O !!!", 2000, "#1b32b4");
+	}
 }
